d3a8d9852167065a0a40bf237501e9baa956a0f8
0str1ch <get@smithed.xyz>
2019-08-29 17:14:25 -0400

    more clean up

--- pages/_document.js
+++ pages/_document.js
@@ -50,60 +50,6 @@ export default class JeremyDev extends Document {
             content="black-translucent"
           />
           <meta name="msapplication-starturl" content="/" />
-          <link
-            rel="preload"
-            href="/static/fonts/MarvinVisionsGX-subset.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preload"
-            href="/static/fonts/NeueMontreal-Regular.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-          <style
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{
-              __html: `
-                      @font-face {
-                        font-family: 'NeueMontreal';
-                        font-display: swap;
-                        src: url('/static/fonts/NeueMontreal-Regular.woff2')
-                          format('woff2');
-                        font-weight: normal;
-                        font-style: normal;
-                        unicode-range: U+000-5FF;
-                      }
-                      @font-face {
-                        font-family: 'NeueMontreal';
-                        font-display: swap;
-                        src: url('/static/fonts/NeueMontreal-Bold.woff2') format('woff2');
-                        font-weight: bold;
-                        font-style: normal;
-                        unicode-range: U+000-5FF;
-                      }
-          
-                      @font-face {
-                        font-family: 'MarvinVisions-Variable';
-                        src: url('/static/fonts/MarvinVisionsGX-subset.woff2')
-                          format('woff2-variations');
-                        font-display: block;
-                        unicode-range: U+000-5FF;
-                        font-weight: 20 170;
-                      }
-          
-          `
-            }}
-          />
-
-          <link
-            rel="manifest"
-            href="/static/manifest.json"
-            crossOrigin="use-credentials"
-          />
         </Head>
         <body>
           <Main />
