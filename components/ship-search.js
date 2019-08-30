35027e6dcbd59468ebbe22ef1b288643637cf7f5
0str1ch <get@smithed.xyz>
2019-08-30 10:59:32 -0400

    cleanup

--- components/ship-search.js
+++ components/ship-search.js
@@ -14,40 +14,11 @@ import {
   PoweredBy
 } from "react-instantsearch-dom";
 import { InstantSearch } from "./instantsearch";
-import CustomPoweredBy from "./custom-powered-by";
-import CustomInfiniteHits from "./custom-infinite-hits";
 import InfiniteHits from "./infinite-scroll-hits";
 
 const searchClient = algoliasearch(
   "LBEZ4EW674",
   "a75591ffd502c0f33a9ba1735b496208"
-);
-
-const HitComponent = ({ hit }) => (
-  <div className="hit">
-    <div>
-      <div className="hit-picture">
-        <img src={hit.media} alt={hit.name} />
-      </div>
-    </div>
-    <div className="hit-content">
-      <div>
-        <Highlight attribute="name" hit={hit} />
-      </div>
-      <div className="hit-description">
-        <Highlight attribute="manufacturer" hit={hit} />
-      </div>
-      <div className="hit-type">
-        <Highlight attribute="focus" hit={hit} />
-      </div>
-      <div className="hit-type">
-        <Highlight attribute="url" hit={hit} />
-        <a href={hit.url} target="_blank" rel="noopener noreferrer">
-          {hit.url}
-        </a>
-      </div>
-    </div>
-  </div>
 );
 
 export default function ShipSearch(props) {
@@ -93,7 +64,7 @@ export default function ShipSearch(props) {
           </style>
         </menu>
         <div>
-          <InfiniteHits hitComponent={HitComponent} />
+          <InfiniteHits />
         </div>
       </content>
       <footer>
