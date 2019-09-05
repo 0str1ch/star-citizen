d78ddbf78a832088c320a3176fe80aed706de0a5
Jeremy Smith <jeremysmith@Jeremys-iMac.fios-router.home>
2019-09-04 23:28:38 -0400

    add next-offline for service worker

--- next.config.js
+++ next.config.js
@@ -1,19 +1,21 @@
 /* eslint-disable import/no-commonjs */
+const withOffline = require("next-offline");
 
-module.exports = {
+module.exports = withOffline({
+  distDir: "build",
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
-      fs: 'empty'
-    }
+      fs: "empty"
+    };
     config.module.rules.push({
       test: /\.css$/,
-      loader: ['style-loader', 'css-loader']
-    })
+      loader: ["style-loader", "css-loader"]
+    });
     if (config.resolve.alias) {
-      delete config.resolve.alias.react
-      delete config.resolve.alias['react-dom']
+      delete config.resolve.alias.react;
+      delete config.resolve.alias["react-dom"];
     }
-    return config
+    return config;
   }
-}
+});
